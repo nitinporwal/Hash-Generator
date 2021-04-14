1dbfe15b0a979120b9508d9b527f76cb4ae6122d
nitin-codr <nitinporwal99@gmail.com>
2021-04-14 07:43:01 +0530

    hash the code

--- app/src/main/java/com/example/hashgenerator/HomeFragment.kt
+++ app/src/main/java/com/example/hashgenerator/HomeFragment.kt
@@ -4,14 +4,17 @@ import android.os.Bundle
 import android.view.*
 import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.example.hashgenerator.databinding.FragmentHomeBinding
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 class HomeFragment : Fragment() {
 
+    private val homeViewModel: HomeViewModel by viewModels()
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
 
@@ -33,14 +36,30 @@ class HomeFragment : Fragment() {
 
 
         binding.generateButton.setOnClickListener {
-            lifecycleScope.launch {
-                applyAnimation()
-                navigateToSuccess()
-            }
+            onGenerateClicked()
         }
 
         return binding.root
 
+    }
+
+    private fun onGenerateClicked() {
+        if(binding.plainText.text.isEmpty()) {
+            showSnackBar("Field Empty.")
+        }
+        else {
+            lifecycleScope.launch {
+                applyAnimation()
+                getHashData()
+                navigateToSuccess()
+            }
+        }
+    }
+
+    private fun getHashData(): String {
+        val algorithm = binding.autoCompleteTextView.text.toString()
+        val plainText = binding.plainText.text.toString()
+        return homeViewModel.getHash(plainText, algorithm)
     }
 
     private suspend fun applyAnimation() {
@@ -71,6 +90,27 @@ class HomeFragment : Fragment() {
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         inflater.inflate(R.menu.home_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == R.id.clear_menu) {
+            binding.plainText.text.clear()
+            showSnackBar("Cleared.")
+            return true
+        }
+        return true
+    }
+
+    private fun showSnackBar(message: String) {
+        val snackBar = Snackbar.make(
+            binding.rootLayout,
+            message,
+            Snackbar.LENGTH_SHORT
+        )
+        snackBar.setAction("Okay") {
+            
+        }
+        snackBar.show()
     }
 
     override fun onDestroyView() {
